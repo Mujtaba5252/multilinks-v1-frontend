3d9d2b76f6d55c506a8bdd68b9279a486e60b7f4
Mujtaba Ali Saif <mujtabaali650@gmail.com>
2023-09-03 18:39:44 +0500

    routes setting

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,17 +1,17 @@
-import { Grid, Text, Title } from '@mantine/core'
-import React from 'react'
+import { Grid, Text, Title } from "@mantine/core";
+import React from "react";
 
 const Login = () => {
   return (
-    <Grid style={{width:"100vw",height:"100vh"}}>
-      <Grid.Col lg={6} sm={12} style={{backgroundColor:"pink"}}>
+    <Grid style={{ width: "100vw", height: "100vh" }}>
+      <Grid.Col lg={6} sm={12} style={{ backgroundColor: "pink" }}>
         <Title order={1}>Login</Title>
       </Grid.Col>
-      <Grid.Col lg={6} sm={12} style={{backgroundColor:"aqua"}}>
+      <Grid.Col lg={6} sm={12} style={{ backgroundColor: "aqua" }}>
         <Title order={1}>happy</Title>
       </Grid.Col>
     </Grid>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
