4afc23360422c75014926856f329ae1f2d07c6c2
Mujtaba Ali <110091001+Mujtaba5252@users.noreply.github.com>
2023-09-22 01:01:33 +0500

    Create ClientViewModal.jsx

---
+++ src/Portals/Staff/ViewClients/ClientViewModal.jsx
@@ -0,0 +1,8 @@
+import { Badge } from "@mantine/core";
+import React from "react";
+
+const ClientViewModal = () => {
+  return <div>Here the client modal data will be placed</div>;
+};
+
+export default ClientViewModal;
