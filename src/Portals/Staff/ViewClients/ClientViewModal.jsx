f92f65791b5c277385521d8dccbae5d2b76a2db0
Mujtaba Ali <110091001+Mujtaba5252@users.noreply.github.com>
2023-09-23 00:39:02 +0500

    Update Client View Modal

--- src/Portals/Staff/ViewClients/ClientViewModal.jsx
+++ src/Portals/Staff/ViewClients/ClientViewModal.jsx
@@ -2,7 +2,15 @@ import { Badge } from "@mantine/core";
 import React from "react";
 
 const ClientViewModal = () => {
-  return <div>Here the client modal data will be placed</div>;
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      Here the client modal data will be placed
+    </div>
+  );
 };
 
 export default ClientViewModal;
