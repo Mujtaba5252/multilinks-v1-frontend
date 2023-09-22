6fc67aa9db4d6dafeeabdd6afc7b3d292502591c
Mujtaba Ali <110091001+Mujtaba5252@users.noreply.github.com>
2023-09-23 00:38:33 +0500

    Update ModalComponnent.jsx

--- src/Components/ModalComponent/ModalComponent.jsx
+++ src/Components/ModalComponent/ModalComponent.jsx
@@ -1,17 +1,40 @@
-import { Button, Container, Group, Modal, createStyles } from "@mantine/core";
+import {
+  Button,
+  Container,
+  Group,
+  Modal,
+  Title,
+  createStyles,
+} from "@mantine/core";
 import React from "react";
+import PageWrapper from "../PageWrapper/PageWrapper";
+import { X } from "tabler-icons-react";
+import { useMediaQuery } from "@mantine/hooks";
 
 const ModalComponent = ({ opened, size, setOpened, children, title }) => {
+  const matches = useMediaQuery("(max-width: 600-1000px)");
+  const ismobile = useMediaQuery("(max-width: 600px)");
   return (
     <Modal
       opened={opened}
       onClose={() => setOpened(false)}
-      withCloseButton={false}
-      title={title}
       centered
+      withCloseButton={true}
       radius={"lg"}
+      title={title}
       size={size}
       styles={{
+        header: {
+          color: "red",
+          textAlign: "center",
+        },
+        title: {
+          paddingLeft: matches ? "50px" : ismobile ? "50px" : "110px",
+          fontSize: "1.2rem",
+          fontWeight: "bold",
+          color: "#0487FF !important",
+        },
+
         overlay: {
           backdropFilter: "blur(3px)",
         },
@@ -19,16 +42,6 @@ const ModalComponent = ({ opened, size, setOpened, children, title }) => {
     >
       <Container p="sm" size={"xl"}>
         {children}
-        <Group pt={"sm"} ml={"auto"} position="right">
-          <Button
-            color="red"
-            variant={"outline"}
-            size="xs"
-            onClick={() => setOpened(false)}
-          >
-            Close
-          </Button>
-        </Group>
       </Container>
     </Modal>
   );
