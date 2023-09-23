762d5d1915785b0886faafe91756d3c5d41521a1
Zain143A <110096531+Zain143A@users.noreply.github.com>
2023-09-23 13:59:54 +0500

    laoder change

--- src/Components/Loader/loader.jsx
+++ src/Components/Loader/loader.jsx
@@ -3,7 +3,6 @@ import { Loader } from "@mantine/core";
 import "./loader.css";
 
 function loader({ color }) {
-function loader() {
   return (
     <div className="loader_div">
       <Loader color={color} className="loader" />
