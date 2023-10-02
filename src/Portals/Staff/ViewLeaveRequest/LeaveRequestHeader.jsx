d3a1542258eb3ea9a092f0acba142386c653cb09
Mujtaba Ali <110091001+Mujtaba5252@users.noreply.github.com>
2023-10-02 21:58:01 +0500

    Leave Request From Staff side

--- src/Portals/Staff/ViewLeaveRequest/LeaveRequestHeader.jsx
+++ src/Portals/Staff/ViewLeaveRequest/LeaveRequestHeader.jsx
@@ -1,3 +1,86 @@
+import { Badge } from "@mantine/core";
+import ActionIcons from "../../../Components/ActionIcons/ActionIcons";
+import { staffRoutes } from "../../../routes";
+import LeaveRequestViewModal from "./LeaveRequestViewModal";
+
 export const LeaveRequestHeader = () => {
-  return [];
+  return [
+    {
+      name: "S.No",
+      selector: (row) => row.sNo,
+      sortable: true,
+      width: "80px",
+    },
+    {
+      name: "ID",
+      selector: (row) => row?.staff_leave_ID || "N/A",
+      width: "150px",
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Name",
+      selector: (row) => row?.staff?.name || "N/A",
+      width: "150px",
+      sortable: true,
+      wrap: true,
+    },
+
+    {
+      name: "Date",
+      selector: (row) => new Date(row.createdAt).toDateString() || "N/A",
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Leave Date",
+      selector: (row) => new Date(row.leave_date).toDateString() || "N/A",
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Subject",
+      selector: (row) => row.subject || "N/A",
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Status",
+      selector: (row) => row.status || "N/A",
+      center: true,
+      cell: (row) => {
+        return (
+          <Badge
+            fullWidth
+            color={
+              row.status === "Approved"
+                ? "green"
+                : row?.status === "Pending"
+                ? "yellow"
+                : "red"
+            }
+          >
+            {row.status}
+          </Badge>
+        );
+      },
+      sortable: true,
+      wrap: true,
+    },
+    {
+      name: "Actions",
+      center: true,
+      cell: (row) => {
+        return (
+          <ActionIcons
+            edit={false}
+            blocked={false}
+            ModalTitle={"Leave Request Details"}
+            ViewModalComponent={<LeaveRequestViewModal row={row} />}
+          />
+        );
+      },
+      sortable: true,
+    },
+  ];
 };
--- src/Portals/Staff/ViewLeaveRequest/ViewLeaveRequest.jsx
+++ src/Portals/Staff/ViewLeaveRequest/ViewLeaveRequest.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageWrapper from "../../../Components/PageWrapper/PageWrapper";
 import { Button, Group } from "@mantine/core";
 import { staffRoutes } from "../../../routes";
 import DataGrid from "../../../Components/DataTable/DataGrid";
 import { LeaveRequestHeader } from "./LeaveRequestHeader";
 import { useNavigate } from "react-router";
+import { axios_get } from "../../../Utils/Axios";
 
 const ViewLeaveRequest = () => {
   const navigate = useNavigate();
+  const [getLeaveData, setLeaveData] = useState([]);
+  const getLeaveRequests = () => {
+    axios_get({ url: "/leave-request", withSNo: true })
+      .then((res) => {
+        console.log(res.data.data);
+        setLeaveData(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getLeaveRequests();
+  }, []);
+
   return (
     <PageWrapper title={"VIEW LEAVE REQUESTS"}>
       {" "}
@@ -18,7 +35,11 @@ const ViewLeaveRequest = () => {
       >
         <Button variant="filled">ADD CLIENT</Button>
       </Group>
-      <DataGrid columns={LeaveRequestHeader()} data={[]} pagination={true} />
+      <DataGrid
+        columns={LeaveRequestHeader()}
+        data={getLeaveData}
+        pagination={true}
+      />
     </PageWrapper>
   );
 };
