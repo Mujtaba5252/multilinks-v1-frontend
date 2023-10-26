796dde5e72ae5d6b517836c77167262f461e801d
Mujtaba Ali <110091001+Mujtaba5252@users.noreply.github.com>
2023-10-26 22:59:28 +0500

    Update FinanceChart.jsx

--- src/Portals/Admin/AdminDashboard/FinanceChart.jsx
+++ src/Portals/Admin/AdminDashboard/FinanceChart.jsx
@@ -2,60 +2,119 @@ import { Card, createStyles } from "@mantine/core";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import Chart from "react-apexcharts";
+import { useNavigate } from "react-router-dom";
 
 const FinanceChart = ({ stats }) => {
-  const [chartData, setChartData] = useState(null);
+  const [chartData, setChartData] = React.useState(null);
+  const navigate = useNavigate();
   const useStyles = createStyles((theme) => ({
     legendColor: theme?.colorScheme === "dark" ? "#FFFFFF" : "#204051",
   }));
 
   const { classes } = useStyles();
-  useEffect(() => {
-    setTimeout(() => {
-      setChartData(options);
-    }, 200);
-  }, [chartData]);
-  let showValue = 10;
+  let showValue = 80.0;
+  const vehicleData = [
+    {
+      name: "New Vehicle",
+      vehicles: stats?.newlyBoughtVehicles ?? showValue,
+      color: "#00a86b",
+      route: "/admin/viewVehiclesInTransit",
+    },
+    {
+      name: "Received Vehicle",
+      vehicles: stats?.vehiclesReceived ?? showValue,
+      color: "#00a86b",
+      route: "/admin/viewVehiclesReceived",
+    },
+
+    {
+      name: "Sold Vehicle",
+      vehicles: stats?.vehiclesSold ?? showValue,
+      color: "#256fff",
+      route: "/admin/viewAllPreInspectionVehicles",
+    },
+
+    {
+      name: "In-Tranist Vehicle",
+      vehicles: stats?.vehiclesInTransit ?? showValue,
+      color: "#256fff",
+      route: "/admin/serviceVehicles",
+    },
+    {
+      name: "In Service Pre-Check",
+      vehicles: stats?.vehiclesInPreInspection ?? showValue,
+      color: "#256fff",
+      route: "/admin/serviceVehicles",
+    },
+    // {
+    //   name: "Pre-Check Completed",
+    //   vehicles: stats?.vehiclesPreInspectionCompleted ?? showValue,
+    //   color: "#F6FA70",
+    //   route: "/admin/viewAllPreInspectionVehicles",
+    // },
+    {
+      name: "In Service Post-Check",
+      vehicles: stats?.vehiclesInPostInspection ?? showValue,
+      color: "#256fff",
+      route: "/admin/viewAllPreInspectionVehicles",
+    },
+    {
+      name: "Service Completed Vehicles",
+      vehicles: stats?.vehiclesServiceCompleted ?? showValue,
+      color: "#704214",
+      route: "/admin/viewAllPreInspectionVehicles",
+    },
+    {
+      name: "Deal Confirmed Vehicles",
+      vehicles: stats?.vehiclesInDealCompletedPhase ?? showValue,
+      color: "#704214",
+      route: "/admin/viewAllPreInspectionVehicles",
+    },
+  ];
 
   const options = {
     height: 500,
     type: "bar",
     options: {
+      // labels: vehicleData?.map((item) => item?.name),
+
+      stacked: true,
+
       chart: {
         toolbar: {
-          show: false,
+          show: true,
         },
         zoom: {
           enabled: true,
         },
+        // events: {
+        //   dataPointSelection: function (
+        //     event,
+        //     chartContext,
+        //     { dataPointIndex }
+        //   ) {
+        //     const { route } = vehicleData[dataPointIndex];
+        //     navigate(route);
+        //   },
+        // },
       },
       // colors: vehicleData?.map((item) => item?.color),
 
       plotOptions: {
         bar: {
-          columnWidth: "35%",
-          distributed: true,
+          columnWidth: "100%",
+          // distributed: true,
         },
       },
       xaxis: {
         type: "category",
-        // categories: vehicleData?.map((item) => item?.name),
+        categories: stats?.MonthWiseData?.map((item) => item.date),
       },
       yaxis: {
-        title: {
-          text: "Months",
-          style: {
-            fontSize: "14px",
-            fontWeight: 600,
-            color: "#7F7F7F",
+        labels: {
+          formatter: function (val) {
+            return val;
           },
-        },
-
-        axisBorder: {
-          show: true,
-          color: "#78909C",
-          offsetX: 0,
-          offsetY: 0,
         },
       },
       legend: {
@@ -65,14 +124,15 @@ const FinanceChart = ({ stats }) => {
         position: "bottom",
         offsetX: 20,
         labels: {
-          // useSeriesColors: false,
           colors: classes.legendColor,
         },
+
         markers: {
           width: 16,
           height: 16,
           radius: 5,
         },
+
         itemMargin: {
           horizontal: 15,
           vertical: 8,
@@ -82,7 +142,7 @@ const FinanceChart = ({ stats }) => {
         type: "solid",
       },
       dataLabels: {
-        enabled: false,
+        enabled: true,
       },
       grid: {
         show: true,
@@ -90,11 +150,44 @@ const FinanceChart = ({ stats }) => {
     },
     series: [
       {
-        name: "Vehicles",
-        // data: vehicleData?.map((item) => item?.vehicles),
+        name: "Client Count",
+        data: stats?.MonthWiseData?.map((item) => item.clientCount),
+      },
+      {
+        name: "Quotation Count",
+        data: stats?.MonthWiseData?.map((item) => item.quotationCount),
+      },
+      {
+        name: "Quotation Grand Total",
+        data: stats?.MonthWiseData?.map((item) => item.quotation_grand_total),
+      },
+      {
+        name: "Invoice Count",
+        data: stats?.MonthWiseData?.map((item) => item.invoiceCount),
+      },
+      {
+        name: "Invoice Amount",
+        data: stats?.MonthWiseData?.map((item) => item.invoice_amount_received),
+      },
+      {
+        name: "Staff Count",
+        data: stats?.MonthWiseData?.map((item) => item.staffCount),
       },
     ],
   };
+  // document
+  //   .querySelectorAll(".apexcharts-legend-text")
+  //   .forEach((label, index) => {
+  //     label.addEventListener("click", () => {
+  //       const { route } = vehicleData[index];
+  //       navigate(route);
+  //     });
+  //   });
+  React.useEffect(() => {
+    setTimeout(() => {
+      setChartData(options);
+    }, 200);
+  }, [chartData]);
   return (
     <Card
       style={{
@@ -105,7 +198,7 @@ const FinanceChart = ({ stats }) => {
         height: "auto",
         position: "relative",
         marginTop: "10px",
-        marginBottom: "10px",
+        marginBottom: "0px",
       }}
     >
       {chartData ? <Chart {...chartData} width="100%" /> : "Loading..."}
